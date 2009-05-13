c80533b0f27c982ae843a21e1249ce8eb5bacc9c
art.clarke <art.clarke@fc9b0e2c-cd67-11dd-bc61-0ff7aae23933>
2009-05-13 01:22:35 +0000

    and update docs

--- java/xuggle-utils/src/com/xuggle/utils/event/Event.java
+++ java/xuggle-utils/src/com/xuggle/utils/event/Event.java
@@ -47,6 +47,19 @@ public abstract class Event implements IEvent
    * Override this method and describe your event.  Make sure
    * you call your super.getDescription() and you'll get nicely
    * formatted event strings.
+   * <p>
+   * If you want to nest another object, the general format an object
+   * log value should take is:
+   * </p>
+   * <code>
+   * classname@uniquecode[field=value;field=value;field=value]
+   * </code>
+   * <p>
+   * For example:
+   * </p>
+   * <code>
+   * com.xuggle.utils.event.Event@16ac176[source=com.xuggle.utils.event.SychronousEventDispatcher@168282d;]
+   * </code>
    * 
    * @return a description of your event of the form "field=value;field=value;"
    */
@@ -58,6 +71,16 @@ public abstract class Event implements IEvent
     return builder.toString();
   }
   
+  /**
+   * Return a string representation of this event.
+   * <p>
+   * You cannot override toString on an Event.  Instead, override
+   * {@link #getDescription()}, and call super.{@link #getDescription()}
+   * before adding your own name/value output pairs.  This way events
+   * will be printed in a standard format.
+   * </p>
+   * @return a string
+   */
   @Override
   public final String toString()
   {
