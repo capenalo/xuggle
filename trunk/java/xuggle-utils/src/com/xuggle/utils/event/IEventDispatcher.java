f9bd8b6f3a410456f04fb8e82d454484502e4168
art.clarke <art.clarke@fc9b0e2c-cd67-11dd-bc61-0ff7aae23933>
2009-08-19 14:05:55 +0000

    Documentation updates

--- trunk/java/xuggle-utils/src/com/xuggle/utils/event/IEventDispatcher.java
+++ trunk/java/xuggle-utils/src/com/xuggle/utils/event/IEventDispatcher.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2008, 2009 by Xuggle Incorporated.  All rights reserved.
+ * Copyright (c) 2008, 2009 by Xuggle Incorporated. All rights reserved.
  * 
  * This file is part of Xuggler.
  * 
  * You can redistribute Xuggler and/or modify it under the terms of the GNU
- * Affero General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
  * 
- * Xuggler is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
- * License for more details.
+ * Xuggler is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with Xuggler.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Xuggler. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.xuggle.utils.event;
@@ -30,37 +29,32 @@ package com.xuggle.utils.event;
  * 
  * <ol>
  * 
- *   <li> Find all appropriate handlers, and call them, stopping
- *      when a handler has successfully handled the event.</li>
- *   <li> If any handler throws an uncaught Exception, we will
- *      dispatch an {@link ErrorEvent} and continue with the
- *      next handler</li>
- *   <li> If the thread a dispatcher is running on is interrupted,
- *      we will return from the current dispatch loop.</li>
- *   <li>If a handler attempts to dispatch an event on the same
- *      dispatcher it is being run from, the new event is queued
- *      until the current handler returns</li>
+ * <li>Find all appropriate handlers, and call them in order
+ * of decreasing priority, stopping when a handler has
+ * successfully handled the event.</li>
+ * <li>If two handlers are registered at the same priority,
+ * their order of execution is undetermined.</li>
+ * <li>If any handler throws an uncaught Exception, {@link IEventDispatcher}
+ * objects will dispatch an {@link ErrorEvent} and continue with the next
+ * handler</li>
+ * <li>If the thread a dispatcher is running on is interrupted,
+ * {@link IEventDispatcher} objects will return in a reasonable amount of time.</li>
+ * <li>Dispatchers must ensure that no handlers registered on that dispatcher
+ * may be called concurrently, even though {@link #dispatchEvent(IEvent)} may be
+ * called concurrently.
+ * 
+ * </li>
  * 
  * </ol>
  * 
- * <p>
- *
- * Dispatchers must ensure that all listeners and handlers are
- * called on the same thread, but that thread need not be the same
- * thread that dispatchEvent() was called on (i.e. a IEventDispatcher
- * may fire up a separate thread for calling listeners and handlers; 
- * in fact, it almost always will).
- * 
- * </p>
- * 
  * @author aclarke
- *
+ * 
  */
-public interface IEventDispatcher extends IEventHandlerRegistrable {
+public interface IEventDispatcher extends IEventHandlerRegistrable
+{
 
   /**
-   * Takes the given event, and passes
-   * it to all appropriate handlers.
+   * Takes the given event, and passes it to all appropriate handlers.
    * 
    * @param event The event to dispatch
    */
